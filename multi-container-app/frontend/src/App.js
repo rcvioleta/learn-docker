b2be85ca7e2b4854c1840e9ba0e2472b15e4edcb
Rogene Violeta (CW) <rvioleta@paypalcorp.com>
2022-10-03 16:03:26 +0800

    Create and display todos

--- multi-container-app/frontend/src/App.js
+++ multi-container-app/frontend/src/App.js
@@ -1,22 +1,43 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState([
+    "Todo 1",
+    "Todo 2",
+    "Todo 3",
+  ]);
+
+  useEffect(() => {
+    // let's fetch todos from our backend!
+    console.log('Component did mount')
+    setTodos(todos)
+
+    return () => {
+      console.log('Component did unmount')
+    }
+  }, [todos]);
+
+  const addTodo = () => {
+    setTodos([...todos, todoInput]);
+    setTodoInput("");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="text" id="Todo-Input" value={todoInput} onChange={(evt) => setTodoInput(evt.target.value)} />
+          <button id="Add-Todo-Button" onClick={addTodo}>Add Todo</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul id="Todo-List">
+          {todos.map((todo, index) => {
+            return (<li key={index} className="Todo-List-Item">{todo}</li>)
+          })}
+        </ul>
       </header>
     </div>
   );
