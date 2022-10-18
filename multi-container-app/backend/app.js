eb941fad3a3717f797241f3c0d0e56b7ce5c89f7
Rogene Cris Violeta <rcvioleta13@gmail.com>
2022-10-18 22:14:37 +0800

    docker-compose setup with docker-compose.yaml. Updated backend and mongodb setup for docker-compose.

--- multi-container-app/backend/app.js
+++ multi-container-app/backend/app.js
@@ -1,27 +1,29 @@
 const express = require('express')
+const cors = require('cors')
 
 const Todos = require('./models/todos.model')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}))
 
 app.get('/todos', async (req, res, next) => {
-  // return res.send("You made it to the /GET route!")
   const todos = await Todos.find({})
   return res.send(todos)
 })
 
 app.post('/todos', async (req, res, next) => {
-  const newTodo = await Todos.create({
-    title: "Learn Docker",
-    description: "This is a top priority!"
-  })
+  const newTodo = await Todos.create(req.body)
   return res.send(newTodo)
 })
 
 app.delete('/todos/:todoId', async (req, res, next) => {
-  await Todos.findByIdAndDelete({ _id: req.params.todoId })
+  const deletedTodo = await Todos.findByIdAndDelete(req.params.todoId)
+  return res.send(deletedTodo)
 })
 
 module.exports = app 
--- multi-container-app/backend/server.js
+++ multi-container-app/backend/server.js
@@ -1,14 +1,16 @@
-const http = require('http')
-const mongoose = require('mongoose')
+const http = require("http");
+const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
 
-const PORT = process.env.PORT ?? 8000
+const PORT = process.env.PORT ?? 8000;
+const { MONGO_DB_UNAME, MONGO_DB_PASS } = process.env;
+const MONGO_DB_URI = `mongodb://${MONGO_DB_UNAME}:${MONGO_DB_PASS}@mongodb:27017/todos?authSource=admin`;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
-mongoose.connect('mongodb://mongodb-container-v2:27071/todos')
+mongoose.connect(MONGO_DB_URI);
