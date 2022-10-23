964c2517a96da9ef415c768530136200afe0da15
Rogene Cris Violeta <rcvioleta13@gmail.com>
2022-10-23 19:15:01 +0800

    docker-compose frontend in docker-compose.yaml

--- multi-container-app/frontend/src/App.js
+++ multi-container-app/frontend/src/App.js
@@ -1,87 +1,103 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-  const [todoInput, setTodoInput] = useState('');
+  const [todoInput, setTodoInput] = useState("");
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    console.log('Component did mount')
-    fetch('http://localhost:8000/todos')
-      .then(res => res.json())
-      .then(data => setTodos(data))
-      .catch(e => console.log('Failed fetching todos!', e))
+    console.log("Component did mount");
+    fetch("http://localhost:8000/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((e) => {
+        console.log("Failed fetching todos!", e);
+      });
 
     return () => {
-      console.log('Component did unmount')
-    }
+      console.log("Component did unmount");
+    };
   }, []);
 
   const addTodo = async () => {
     try {
-      const req = await fetch('http://localhost:8000/todos', {
-        method: 'post',
+      const req = await fetch("http://localhost:8000/todos", {
+        method: "post",
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           title: todoInput,
-          description: "Lorem ipsum!"
-        })
-      })
+          description: "Lorem ipsum!",
+        }),
+      });
 
-      const newTodo = await req.json()
-      setTodos([...todos, newTodo])
-      setTodoInput('')
+      const newTodo = await req.json();
+      setTodos([...todos, newTodo]);
+      setTodoInput("");
     } catch (e) {
-      console.log('Unable to add new todo!', e)
+      console.log("Unable to add new todo!", e);
     }
-  }
+  };
 
   const deleteTodo = async (todoId) => {
     try {
       await fetch(`http://localhost:8000/todos/${todoId}`, {
-        method: 'delete',
+        method: "delete",
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        }
-      })
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 
-
-      setTodos([...todos, newTodo])
+      setTodos((prevTodos) => {
+        return prevTodos.filter((todo) => todo._id !== todoId);
+      });
     } catch (e) {
-      console.log('Unable to delete todo!', e)
+      console.log("Unable to delete todo!", e);
     }
-  }
+  };
 
   const deleteBtnStyle = {
-    textDecoration: 'none',
-    color: 'red'
-  }
+    textDecoration: "none",
+    color: "red",
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <input type="text" id="Todo-Input" value={todoInput} onChange={(evt) => setTodoInput(evt.target.value)} />
-          <button id="Add-Todo-Button" onClick={addTodo}>Add Todo</button>
+          <input
+            type="text"
+            id="Todo-Input"
+            value={todoInput}
+            onChange={(evt) => setTodoInput(evt.target.value)}
+          />
+          <button id="Add-Todo-Button" onClick={addTodo}>
+            Add Todo
+          </button>
         </p>
         <ul id="Todo-List">
           {todos.map((todo) => {
             return (
               <li key={todo._id} className="Todo-List-Item">
                 {todo.title}&nbsp;&nbsp;
-                <a href='#' style={deleteBtnStyle} onClick={() => deleteTodo(todo._id)}>x</a>
+                <a
+                  href="#"
+                  style={deleteBtnStyle}
+                  onClick={() => deleteTodo(todo._id)}
+                >
+                  x
+                </a>
               </li>
-            )
+            );
           })}
         </ul>
       </header>
-    </div >
+    </div>
   );
 }
 
